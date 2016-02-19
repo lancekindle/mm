1ced5be060c41e5990006c57d6eebc6b304f63cf
Lance Kindle <lance.kindle@gmail.com>
2016-02-19 11:11:18 -0800

    add commonly-used linux apps

---
+++ misc/linux_apps.mm
@@ -0,0 +1,88 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Applications&#xa;used in Linux" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1455908328500"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="12"/>
+<node TEXT="Documents" POSITION="right" ID="ID_647654841" CREATED="1455908550131" MODIFIED="1455908673495">
+<edge COLOR="#ff00ff"/>
+<node TEXT="PDF" ID="ID_1051776205" CREATED="1455908332344" MODIFIED="1455908572609">
+<node TEXT="xournal  -- Write/annotate pdfs. Really easy with a tablet" ID="ID_434637659" CREATED="1455908365290" MODIFIED="1455908400438"/>
+</node>
+<node TEXT="Excel / Word" ID="ID_1339212618" CREATED="1455908575291" MODIFIED="1455908585162">
+<node TEXT="libre-office   --   excellent document editor" ID="ID_64461476" CREATED="1455908585824" MODIFIED="1455908607262"/>
+</node>
+<node TEXT="eBooks" ID="ID_1131671596" CREATED="1455908674436" MODIFIED="1455908683715">
+<node TEXT="Calibre   --   works with any ereader, and can convert between formats" ID="ID_964610589" CREATED="1455908684803" MODIFIED="1455908704335"/>
+</node>
+<node TEXT="Text Editor" ID="ID_1705641148" CREATED="1455908783390" MODIFIED="1455908824674">
+<node TEXT="vim   --   for editing code / plaintext files. Very extensible" ID="ID_1343219244" CREATED="1455908827533" MODIFIED="1455908857471"/>
+<node TEXT="haroopad   --   edit markdown documents. Includes live-preview" ID="ID_1114147174" CREATED="1455908857905" MODIFIED="1455908883917"/>
+</node>
+</node>
+<node TEXT="CD / DVD" POSITION="right" ID="ID_1909422048" CREATED="1455908404968" MODIFIED="1455908409493">
+<edge COLOR="#0000ff"/>
+<node TEXT="k3b  -- best software to burn dvds" ID="ID_45496276" CREATED="1455908410863" MODIFIED="1455908429319"/>
+</node>
+<node TEXT="Video" POSITION="right" ID="ID_695029182" CREATED="1455908719574" MODIFIED="1455908724437">
+<edge COLOR="#7c007c"/>
+<node TEXT="vlc   --   &quot;just works&quot; and can play anything video/audio" ID="ID_217064867" CREATED="1455908439917" MODIFIED="1455908727307"/>
+</node>
+<node TEXT="Music / Audio" POSITION="right" ID="ID_1457427690" CREATED="1455908730856" MODIFIED="1455908736390">
+<edge COLOR="#7c7c00"/>
+<node TEXT="vlc   --   &quot;just works&quot; and can play anything video/audio" ID="ID_1078828039" CREATED="1455908439917" MODIFIED="1455908727307"/>
+<node TEXT="Audacity   --   easy to use sound editor" ID="ID_366539966" CREATED="1455908745571" MODIFIED="1455908759696"/>
+</node>
+<node TEXT="Images" POSITION="right" ID="ID_365396162" CREATED="1455908612977" MODIFIED="1455908618587">
+<edge COLOR="#007c00"/>
+<node TEXT="GIMP   --   best image editor &quot;photoshop&quot; quality" ID="ID_1593319845" CREATED="1455908619191" MODIFIED="1455908634920"/>
+<node TEXT="Inkscape   --   svg editor and design editor. For logos and such" ID="ID_1323524959" CREATED="1455908640107" MODIFIED="1455908662721"/>
+</node>
+</node>
+</map>
