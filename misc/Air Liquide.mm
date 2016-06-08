d1de4494b9c1a7793b02698c40cfb5ee5bab5e45
Lance Kindle <lance.kindle@gmail.com>
2016-06-07 21:10:28 -0700

    add air liquide experiences

---
+++ misc/Air Liquide.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Air Liquide" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1459204094015"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="11"/>
+<node TEXT="IT support experience" POSITION="left" ID="ID_984940357" CREATED="1460998821948" MODIFIED="1460998841948">
+<edge COLOR="#007c7c"/>
+<node TEXT="Setup Norton Antivirus on ~ 12 on-site desktop computers" ID="ID_1202944976" CREATED="1460998864242" MODIFIED="1460998894717">
+<node TEXT="In at least one case I had to fiddle with registry key in order to uninstall and reinstall.&#xa;It was an ugly hack that was necessary because we lost the password" ID="ID_788237878" CREATED="1460998898619" MODIFIED="1460998944412"/>
+</node>
+<node TEXT="Recognized, backed-up, and replaced a failing Network-Attached Hard-drive (NAS)" ID="ID_1569936180" CREATED="1460998948123" MODIFIED="1460998976373"/>
+<node TEXT="Found and replaced an intermittently-bad ethernet cable" ID="ID_99277313" CREATED="1460998977261" MODIFIED="1460999002146"/>
+<node TEXT="Wrote a program to auto-restart critical computer software when it&apos;s accidentally closed by employees" ID="ID_1202429701" CREATED="1460999126427" MODIFIED="1460999971071"/>
+</node>
+<node TEXT="experience gained during air liquide" POSITION="right" ID="ID_1104447412" CREATED="1459204095023" MODIFIED="1459204101596">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Terminology:&#xa;SMR - Steam Methane Reformer&#xa;LH2 - Liquid Hydrogen&#xa;LN2 - Liquid Nitrogen&#xa;DI - De-Ionized" POSITION="right" ID="ID_281903100" CREATED="1459204290203" MODIFIED="1459204402997">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="facility had two SMRs, eight Gas Chromatographs, five compressors, three dryers, two glycol / water cooling systems, four miniature SMRs (H2Gens), three 20k? LH2 tanks, two 15k and one 10k and one 50k LN2 tanks, one DI water loop, and all the support systems that revolved around it. We also had a high-pressure N2 system with a 10k tank and a LIN cryo-pump, and a high-pressure tube trailer" POSITION="right" ID="ID_962344259" CREATED="1459204102198" MODIFIED="1459210424720">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="45k SMR" POSITION="right" ID="ID_1919550453" CREATED="1459204456704" MODIFIED="1459204464425">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="60k SMR" POSITION="right" ID="ID_1835196797" CREATED="1459204464865" MODIFIED="1459204468099">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="H2Gens" POSITION="right" ID="ID_640226578" CREATED="1459204468684" MODIFIED="1459204510300">
+<edge COLOR="#ffff00"/>
+</node>
+<node TEXT="DI Water Loop" POSITION="right" ID="ID_1850727627" CREATED="1459204471932" MODIFIED="1459204484671">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Plant 3 Cooling Loop" POSITION="right" ID="ID_1567449309" CREATED="1459204485136" MODIFIED="1459204494665">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Plant 4 Cooling Loop" POSITION="right" ID="ID_1626392463" CREATED="1459204495081" MODIFIED="1459204499406">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="Gas Chromatographs" POSITION="right" ID="ID_237900726" CREATED="1459204500049" MODIFIED="1459204555054">
+<edge COLOR="#7c007c"/>
+<node TEXT="Gas Chromatographs (GC&apos;s) were obtained to monitor the purity of our delivered H2 gas. We used a set of two GC&apos;s for each Hydrogen production system. Each GC was responsible for separating the Hydrogren gas into H2, N2, and CO. GC#1 would monitor N2 purity, while GC#2 would monitor CO purity, as each had to report less than 1.5 ppm of said gas.&#xa;Since we had four H2Gen&apos;s, we setup a system to total reported gas purity of the H2Gens, with the ability to individually monitor a specific H2Gen&apos;s purity. Then we had a pair of GC&apos;s monitoring the 60k&apos;s output, and a pair monitoring the 45k&apos;s output. Finally we had a pair of GC&apos;s which allowed us to monitor the overall pipeline purity in case any particular GC pair went defunct. The pipeline GC&apos;s also had the option to instead monitor the 45k, since those GC&apos;s were oldest and notoriously spotty." ID="ID_1359913095" CREATED="1459204556136" MODIFIED="1459205656206"/>
+<node TEXT="GC&apos;s work by diffusing the gases during several loops and diverting / venting gas flows at key times to separate gases, until the final gas mixture running through the heated mercury bed is Helium and Nitrogen (or CO). A light emitter and light sensor then sense the varying absorption levels of the mercury vapor to determine the concentration. Due to this separation process based on timing, the concentration is measured in intervals. Not continuously. Each sampling interval is about 5 minutes." ID="ID_1936907" CREATED="1459205883421" MODIFIED="1459206477676"/>
+<node TEXT="piping is all tiny, tiny 1/8th or 1/16th piping, and a minimal number of parts are used to reduce cost and complexity of the device. Mechanical devices like tiny needle-valves controlling the rate of gas venting and introduction, combined with a heated coiled mass of 1/8th tubing with some catalyst/compound to diffuse the gas, and even a simple 1/8th tube crimped and venting to atmosphere to control the rate of gas flow. All these factors combine to make a device that requires device-specific calibration and fiddling. But once it works, it&apos;ll generally continue to work, until the diverting valve starts grinding, or the heating element (for the catalyst bed) starts failing, or the UV-lamp for the mercury bed starts failing." ID="ID_1500345721" CREATED="1459206044446" MODIFIED="1459206334054"/>
+<node TEXT="Helium is the primary &quot;neutral&quot; gas used to drive the flow of hydrogen, CO, and N2. As the gas is separated, to keep the volume of gas constant, helium is introduced into the mix. The special diverting valves (two of them). Alternate between sending as though the catalyst bed to venting. Monthly maintenance involves sending pure Helium through the GC catalyst bed to drive out absorbed gases and &quot;purify&quot; the bed. Without this, the bed would eventually saturate and be unable to effectively separate the gases." ID="ID_1385079224" CREATED="1459206334608" MODIFIED="1459206550705"/>
+<node TEXT="Helium is obtained from pressurized Helium bottles. Usually from 3000PSI bottles, which is immediately reduced to 120PSI, and later further reduced to 30? PSI." ID="ID_1724475579" CREATED="1459206561886" MODIFIED="1459206644312"/>
+<node TEXT="Reducing the pressure down 2 times immensely helps with controlling pressure. Otherwise directly reducing from 3000PSI to 30PSI might experience blips as the pressure-controlling valve adjusts around it&apos;s setpoint. Each Pressure-Controlling valve is used in it&apos;s stable range. The 3k one can adjust with accuracy in the 100&apos;s. The 200PSI one can adjust with acuracy in the 1&apos;s." ID="ID_56200835" CREATED="1459206644773" MODIFIED="1459206777917"/>
+<node TEXT="We also found that helium bottles seemed to produce bad-quality helium after it dropped to 300PSI or so. As a result, we&apos;d replace the bottles once they dropped to 400PSI" ID="ID_161008658" CREATED="1459206778420" MODIFIED="1459206834243"/>
+</node>
+</node>
+</map>
